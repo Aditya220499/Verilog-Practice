f03f992eb9c18cc6b309c71f6c06ad679213aa89
Aditya Lonkar <adityalonkar99@gmail.com>
2025-09-28 22:21:09 +0530

    Full adder created

--- Adder-Subtractor/full_adder.v
+++ Adder-Subtractor/full_adder.v
@@ -1,3 +1,15 @@
+`include "half_adder.v"
 module full_adder(
-    output 
-)
+    output wire sum,
+    output wire carry,
+    input wire a,
+    input wire b,
+    input wire cin
+);
+wire ha1_sum, ha1_carry, ha2_sum, ha2_carry ;
+
+half_adder ha1(ha1_sum, ha1_carry, a, b );
+half_adder ha2(sum, ha2_carry, ha1_sum, cin );
+assign carry = ha1_carry | ha2_carry;
+
+endmodule
--- Adder-Subtractor/half_adder.v
+++ Adder-Subtractor/half_adder.v
@@ -1,8 +1,8 @@
 module half_adder(
-    output sum,
-    output carry,
-    input a,
-    input b
+    output wire sum,
+    output wire carry,
+    input wire a,
+    input wire b
 );
     assign sum = a ^ b;      // Sum is the XOR of inputs
     assign carry = a & b;    // Carry is the AND of inputs
