70f752884949b6602def6da0d7524606b1f695fb
Aditya Lonkar <adityalonkar99@gmail.com>
2025-09-29 07:56:24 +0530

    Added todo logic

--- Adder-Subtractor/complement_2s.v
+++ Adder-Subtractor/complement_2s.v
@@ -2,5 +2,6 @@ module complement_2s(
     output wire compVal,
     input wire val
 );
-
+// Add 2 comp logic here 
+//Can add vectors of differenntsize , but better to do padding manually
 endmodule
