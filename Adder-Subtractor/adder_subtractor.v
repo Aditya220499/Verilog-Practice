a36c859d340454cf1c92c77b44268c2e86b6a418
Aditya Lonkar <adityalonkar99@gmail.com>
2025-09-30 22:32:54 +0530

    check

--- Adder-Subtractor/adder_subtractor.v
+++
@@ -1,8 +0,0 @@
-module adder_subtractor(
-    output wire result,
-    output wire sign,     // 0 = Positive , 1 = Negative
-    input wire a,
-    input wire b,
-    input wire mode,   //0 = Add , 1 = Sub
-);
-endmodule
