f066723aadcd77a545334bd776c6a9201ad75505
Aditya Lonkar <adityalonkar99@gmail.com>
2025-10-01 22:01:41 +0530

    adder-sub logic

---
+++ Adder-Subtractor/adder_subtractor.v
@@ -0,0 +1,9 @@
+module adder_subtractor(
+    output wire [3:0]sum,
+    output wire carry_out,
+    input wire [3:0]a,
+    input wire [3:0]b,
+    input wire cin
+)
+
+endmodule
--- Adder-Subtractor/complement_2s.v
+++ Adder-Subtractor/complement_2s.v
@@ -2,7 +2,5 @@ module complement_2s(
     output wire compVal,
     input wire val
 );
-// Add 2 comp logic here 
-//Can add vectors of differenntsize , but better to do padding manually
     assign compVal = ~val +1'b1;
 endmodule
