37393d244467ee043f835bc4e75d30040af11b40
Aditya Lonkar <adityalonkar99@gmail.com>
2025-10-01 21:55:16 +0530

    rca adder logic

--- Adder-Subtractor/ripple_carry_adder.v
+++ Adder-Subtractor/ripple_carry_adder.v
@@ -1,3 +1,4 @@
+`include "full_adder.v"
 module ripple_carry_adder(
     output wire [3:0]sum,
     output wire carry_out,
@@ -5,11 +6,12 @@ module ripple_carry_adder(
     input wire [3:0]b,
     input wire cin
 );
-//add logic
     wire [4:0]c;
-    full_adder(sum, carry, a, b , cin);
-    full_adder(sum, carry, a, b , cin);
-    full_adder(sum, carry, a, b , cin);
-    full_adder(sum, carry, a, b , cin);
+    assign c[0] = cin;
+    full_adder fa1(sum[0], c[1], a[0], b[0] , c[0]);
+    full_adder fa2(sum[1], c[2], a[1], b[1] , c[1]);
+    full_adder fa3(sum[2], c[3], a[2], b[2] , c[2]);
+    full_adder fa4(sum[3], c[4], a[3], b[3] , c[3]);
+    assign carry_out = c[4];
 
 endmodule
