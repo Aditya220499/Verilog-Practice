e011a026bc6d959c5412ff08b9a522a000152d00
Aditya Lonkar <adityalonkar99@gmail.com>
2025-10-02 08:47:10 +0530

    error removal

--- Adder-Subtractor/ripple_carry_adder.v
+++ Adder-Subtractor/ripple_carry_adder.v
@@ -8,7 +8,6 @@ module ripple_carry_adder(
 );
     wire [4:0]c;
     assign c[0] = cin;
-    if (cin) assign b = ~b;
     full_adder fa1(sum[0], c[1], a[0], b[0] , c[0]);
     full_adder fa2(sum[1], c[2], a[1], b[1] , c[1]);
     full_adder fa3(sum[2], c[3], a[2], b[2] , c[2]);
